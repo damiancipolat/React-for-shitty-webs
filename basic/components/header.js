2e628ae5e21471fc66cb04c4867b30bce8bbc053
Damian Cipolat <damian.cipolat@gmail.com>
2019-07-24 22:17:16 -0300

    add

--- basic/components/header.js
+++ basic/components/header.js
@@ -7,8 +7,9 @@ class Header extends React.Component {
   render() {
 
      return (<div className="header">
-              <h1>My Website</h1>
-              <p>Resize the browser window to see the effect.</p>
+     						<img className="logo" src="./resources/react.png"/>
+              	<h1>My Website</h1>
+              	<p>Resize the browser window to see the effect.</p>
              </div>);
 
   }
